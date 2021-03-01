3ed4e74b0901dad733c31d59eb15fd9d511e1e80
Serhii Larchenko <serhii@sweetcode.pro>
2021-03-01 13:40:09 +0200

    develop added nextjs config

---
+++ front-end/next.config.js
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withImages = require('next-images');
+const withPlugins = require('next-compose-plugins');
+
+const nextConfig = {
+  env: {
+    API_URL: process.env.API_URL,
+  },
+};
+
+const imagesConfig = {
+  // bitmap images should be imported like path
+  fileExtensions: ['jpg', 'jpeg', 'png', 'webp'],
+  // svg files should be imported like React component
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ['@svgr/webpack', 'url-loader'],
+    });
+
+    return config;
+  },
+};
+
+const plugins = [withImages(imagesConfig)];
+
+module.exports = withPlugins(plugins, nextConfig);
