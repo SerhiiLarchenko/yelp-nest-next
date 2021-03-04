c3f1d1c0d64636b3f0148980a4e9ddfc59c2ef2e
Raven <r.volianiuk@gmail.com>
2021-03-04 13:20:23 +0200

    map marker was changed

--- front-end/components/common/google-map/marker/styles.ts
+++ front-end/components/common/google-map/marker/styles.ts
@@ -4,15 +4,14 @@ const hoverStyles = css`
   transform: translate(-50%, -50%) scale(1.2);
 `;
 
-const Marker = styled.div<{ url: string; isCardHovered?: boolean }>`
+const OuterMarker = styled.div<{ isCardHovered?: boolean }>`
   position: absolute;
   transform: translate(-50%, -50%);
-  width: 40px;
-  height: 40px;
+  width: 55px;
+  height: 55px;
   border-radius: 100%;
+  background-color: transparent;
   user-select: none;
-  background-image: url(${(p) => p.url});
-  background-size: cover;
   cursor: pointer;
   border: 2px solid ${(p) => p.theme.colors.primary};
   transition: ${(p) => p.theme.transition.default};
@@ -28,4 +27,19 @@ const Marker = styled.div<{ url: string; isCardHovered?: boolean }>`
     `}
 `;
 
-export { Marker };
+const InnerMarker = styled.div<{ url: string }>`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 40px;
+  height: 40px;
+  border-radius: 100%;
+  user-select: none;
+  background-image: url(${(p) => p.url});
+  background-size: cover;
+  cursor: pointer;
+  transition: ${(p) => p.theme.transition.default};
+`;
+
+export { InnerMarker, OuterMarker };
